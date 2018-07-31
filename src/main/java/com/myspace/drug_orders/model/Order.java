79f3c090f64c416a8669b87b6fdc7cf53c0ea4e9
pamadmin <>
2018-07-31 14:17:45 +0000

    {/src/main/java/com/myspace/drug_orders/model/Order.java}

--- src/main/java/com/myspace/drug_orders/model/Order.java
+++ src/main/java/com/myspace/drug_orders/model/Order.java
@@ -6,12 +6,25 @@ package com.myspace.drug_orders.model;
 
 public class Order implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Order() {
-    }
+	@org.kie.api.definition.type.Label(value = "Order Line Items")
+	private java.util.List<java.lang.String> orderLineItems;
 
+	public Order() {
+	}
 
+	public java.util.List<java.lang.String> getOrderLineItems() {
+		return this.orderLineItems;
+	}
 
+	public void setOrderLineItems(
+			java.util.List<java.lang.String> orderLineItems) {
+		this.orderLineItems = orderLineItems;
+	}
+
+	public Order(java.util.List<java.lang.String> orderLineItems) {
+		this.orderLineItems = orderLineItems;
+	}
 
 }
